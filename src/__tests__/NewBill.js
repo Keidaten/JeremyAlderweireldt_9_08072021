5236512341c1130b5d51a887b1cbabe20a6ebb6b
Jey <jeremyalder@hotmail.fr>
2021-07-21 19:52:09 +0200

    [Ajout de tests]container/NewBill handleChangeFile

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,53 @@
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { screen, fireEvent } from '@testing-library/dom';
+import NewBillUI from '../views/NewBillUI.js';
+import NewBill from '../containers/NewBill.js';
+import { localStorageMock } from '../__mocks__/localStorage.js';
+import { ROUTES } from '../constants/routes.js';
 
+describe('Given I am connected as an employee', () => {
+	describe('When I am on NewBill Page and I adding a file', () => {
+		test('Then a file should be added to the input', () => {
+			//Defini le localStorage
+			Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+			window.localStorage.setItem(
+				'user',
+				JSON.stringify({
+					type: 'Employee',
+				})
+			);
+			//Template html
+			const html = NewBillUI();
+			document.body.innerHTML = html;
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+			// Navigation
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname });
+			};
+
+			//Firestore = null
+			const firestore = null;
+
+			//Bill initialisation
+			const newNewBill = new NewBill({
+				document,
+				onNavigate,
+				firestore,
+				localStorage: window.localStorage,
+			});
+
+			//Tests
+			const handleChangeFile = jest.fn(() => newNewBill.handleChangeFile);
+			const file = screen.getByTestId('file');
+			file.addEventListener('input', handleChangeFile);
+
+			fireEvent.input(file, {
+				target: {
+					files: [new File(['file.png'], 'file.png', { type: 'image/png' })],
+				},
+			});
+
+			expect(file.files[0].name).toBe('file.png');
+			expect(handleChangeFile).toHaveBeenCalled();
+		});
+	});
+});
