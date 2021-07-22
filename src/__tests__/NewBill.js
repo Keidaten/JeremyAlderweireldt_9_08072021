583e5b938928c5d0f495fa88c7c87db03b3d1d13
Jey <jeremyalder@hotmail.fr>
2021-07-22 15:08:49 +0200

    [Ajout de tests]container/NewBill handleSubmit

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,8 +5,8 @@ import { localStorageMock } from '../__mocks__/localStorage.js';
 import { ROUTES } from '../constants/routes.js';
 
 describe('Given I am connected as an employee', () => {
-	describe('When I am on NewBill Page and I adding a file', () => {
-		test('Then a file should be added to the input', () => {
+	describe("When I am on NewBill Page and I'm adding a valid file", () => {
+		test('Then this file should be added to the input', () => {
 			//Defini le localStorage
 			Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 			window.localStorage.setItem(
@@ -48,6 +48,91 @@ describe('Given I am connected as an employee', () => {
 
 			expect(file.files[0].name).toBe('file.png');
 			expect(handleChangeFile).toHaveBeenCalled();
+		});
+	});
+	describe('When I am on NewBill Page and I submit the form with an invalid file extension', () => {
+		test('Then we should stay on the same page', () => {
+			//Defini le localStorage
+			Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+			window.localStorage.setItem(
+				'user',
+				JSON.stringify({
+					type: 'Employee',
+				})
+			);
+			//Template html
+			const html = NewBillUI();
+			document.body.innerHTML = html;
+
+			// Navigation
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname });
+			};
+
+			//Firestore = null
+			const firestore = null;
+
+			//Bill initialisation
+			const newNewBill = new NewBill({
+				document,
+				onNavigate,
+				firestore,
+				localStorage: window.localStorage,
+			});
+
+			//Tests
+			const handleSubmit = jest.fn(() => newNewBill.handleSubmit);
+
+			newNewBill.fileName = 'Invalid format';
+
+			const form = screen.getByTestId('form-new-bill');
+			form.addEventListener('submit', handleSubmit);
+			fireEvent.submit(form);
+
+			expect(newNewBill.fileName).toBe('Invalid format');
+			expect(screen.getByText('Envoyer une note de frais')).toBeTruthy;
+			expect(handleSubmit).toHaveBeenCalled();
+		});
+	});
+	describe('When I am on NewBill Page and I submit the form with a valid file extension', () => {
+		test('Then page should change and bring us back to dashboard', () => {
+			//Defini le localStorage
+			Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+			window.localStorage.setItem(
+				'user',
+				JSON.stringify({
+					type: 'Employee',
+				})
+			);
+			//Template html
+			const html = NewBillUI();
+			document.body.innerHTML = html;
+
+			// Navigation
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname });
+			};
+
+			//Firestore = null
+			const firestore = null;
+
+			//Bill initialisation
+			const newNewBill = new NewBill({
+				document,
+				onNavigate,
+				firestore,
+				localStorage: window.localStorage,
+			});
+
+			//Tests
+			const handleSubmit = jest.fn(() => newNewBill.handleSubmit);
+
+			const form = screen.getByTestId('form-new-bill');
+			form.addEventListener('submit', handleSubmit);
+			fireEvent.submit(form);
+
+			expect(screen.getByText('Mes notes de frais')).toBeTruthy;
+			expect(handleSubmit).toHaveBeenCalled();
 		});
 	});
 });
